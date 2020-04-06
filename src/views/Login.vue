89ee24167823abc97d8dc1fb263fc508c83dc81c
VIRGO96 <dawoodahmed696@gmail.com>
2020-04-06 11:45:26 +0500

    login view

---
+++ src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <LoginHeader />
+    <b-row>
+       <b-col md="4" class="offset-md-2">
+        <LoginSecondaryHeader/>
+
+       </b-col>
+       <b-col>
+         <b-container class="  mt--50" >
+          <b-row>
+            <b-col md="12" class="text-left text-white">
+               <h3>FADV Management</h3> 
+            </b-col>
+            <b-col md="12" class="mt-3">
+                <img align="left" src="../assets/logo.png" alt="">
+            </b-col>
+            
+          </b-row>
+          
+    </b-container>
+
+       </b-col>
+    </b-row>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginHeader from '@/components/LoginHeader.vue'
+import LoginSecondaryHeader from '@/components/LoginSecondaryHeader.vue'
+
+export default {
+    name: 'Users',
+    components: {
+        LoginHeader,
+        LoginSecondaryHeader
+    },
+    data() {
+        return {
+      }
+    }
+}
+</script>
+
+<style scoped>
+.breadcrumb {
+  padding-left:0rem !important;
+  padding-top:0rem !important
+
+}
+
+
+</style>
