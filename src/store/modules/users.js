a676adb848247060c60269440a1bbc385a271075
VIRGO96 <dawoodahmed696@gmail.com>
2020-04-21 17:11:38 +0500

    users store

---
+++ src/store/modules/users.js
@@ -0,0 +1,56 @@
+const state = {
+    loggedUser: null,
+    allusers:[],
+    notifications:{},
+
+};
+
+const getters = {
+    loggedUser:state => state.loggedUser,
+    notifications:state => state.notifications,
+    allusers:state => state.allusers,
+    users: (state) => {
+        return state.allusers.filter(item=>item.UserRoleCode=='USER')
+    },
+    employer: (state) => {
+        return state.allusers.filter(item=>item.UserRoleCode=='EMPLOYER')
+    }
+    
+};
+
+const actions = {
+};
+const mutations = {
+    setLoggedUser: (state, payload) => {
+        console.log("auth user set")
+        if(localStorage.getItem('uscreen-token')==null){
+            localStorage.setItem("uscreen-token",JSON.stringify(payload.Token))
+
+        }
+        localStorage.setItem("loggedUser",JSON.stringify(payload))
+
+        state.loggedUser=payload
+
+    },
+    setAllUsers(state,payload){
+        state.allusers=payload
+        
+    },
+    setNotifications:(state,payload) =>{
+        state.notifications=payload
+    },
+    logout(state,payload){
+        state.loggedUser=null;
+        localStorage.removeItem('uscreen-token')
+        localStorage.removeItem('loggedUser')
+
+    }
+    
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
