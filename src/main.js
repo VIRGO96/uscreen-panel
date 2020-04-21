ea640124874b201643de20aba3dd4363dfa66327
VIRGO96 <dawoodahmed696@gmail.com>
2020-04-21 17:13:41 +0500

    json and toast css

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import VueApexCharts from 'vue-apexcharts'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'native-toast/dist/native-toast.css'
+
+import JsonCSV from 'vue-json-csv'
+ 
+Vue.component('downloadCsv', JsonCSV)
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(VueApexCharts)
