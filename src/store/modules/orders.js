33fa2a48b0178b1d320c6c0ccf1c93891b1825a1
VIRGO96 <dawoodahmed696@gmail.com>
2020-05-13 15:37:25 +0500

    order page

--- src/store/modules/orders.js
+++ src/store/modules/orders.js
@@ -1,10 +1,11 @@
 const state = {
     allorders:[],
-
+    orderpage:0,
 };
 
 const getters = {
     allorders:state => state.allorders,
+    orderpage:state => state.orderpage,
     
 };
 
@@ -13,8 +14,14 @@ const actions = {
 const mutations = {
   
     setAllOrders(state,payload){
-        state.allorders=payload
-    }
+        state.allorders=payload.PageData
+        state.orderpage=payload.HitsTotal
+
+    },
+    setOrderCount(state,payload){
+        state.orderpage=payload.HitsTotal
+
+    },
     
 };
 
