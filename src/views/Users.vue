1e8e97a3d026daddd226d84bccb3899813d48e25
VIRGO96 <dawoodahmed696@gmail.com>
2020-05-14 20:00:01 +0500

    users new workin

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,39 +1,11 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <Header msg="Welcome to Your Vue.js App"/>
     <SecondaryHeader msg="Welcome to Your Vue.js App"/>
-    <!-- <b-container class="pr-3 pt-3  pl-3  pb-1 bg-white card shadow" style="margin-top:-50px;border:1px solid #dcdcdc">
-      <b-row>
-         <b-col>
-           <div class="text-left text-light-gray"> <h5>Members</h5> </div>
-           <div>
-            <b-breadcrumb class="bg-white">
-              <b-breadcrumb-item href="">
-                <span class="fa fa-home"></span>
-              </b-breadcrumb-item>
-              <b-breadcrumb-item href="" >UI</b-breadcrumb-item>
-              <b-breadcrumb-item active>List Groups</b-breadcrumb-item>
-
-            </b-breadcrumb>
-           </div>
-         </b-col>
-         <b-col md="2">
-           <div style="margin-top:-30px; !important">
-              <apexchart type="bar" height="100" :options="chartOptions" :series="series"></apexchart>
-          </div>
-         </b-col>
-          <b-col md="2">
-            <div style="margin-top:-30px; !important">
-                   <apexchart type="bar" height="100" :options="secchartOptions" :series="series"></apexchart>
-            </div>
-         </b-col>
-      </b-row>
-    </b-container> -->
     <b-container class="card bg-white mt-2 pb-5 pt-2">
       <div class="text-md-left">
         <b-button @click="active_user='Employers'"  pill :variant="active_user=='Employers' ? 'primary':'outline-primary'" class="pl-5 pr-5" >Employers</b-button>
-        <b-button @click="active_user='Individuals';totalRows=users.length" class="mt-xs-1 ml-md-2 pl-5 pr-5" pill :variant="active_user=='Individuals' ? 'primary':'outline-primary'" >Individuals</b-button>
+        <b-button @click="active_user='Individuals';fetchUsers(1)" class="mt-xs-1 ml-md-2 pl-5 pr-5" pill :variant="active_user=='Individuals' ? 'primary':'outline-primary'" >Individuals</b-button>
       </div>
       <div class="mt-2 text-md-left text-primary">
        <h4 >Filtering</h4> 
@@ -42,12 +14,11 @@
         <b-row>
            <b-col md="6" class="text-md-left">
              <div class="d-block d-md-flex">
-                <b-form-checkbox class="mt-xs-1 self-center">
+                <b-form-checkbox v-model="search_params.company_flag" class="mt-xs-1 self-center">
                 Company
               </b-form-checkbox>
-              <select name="" class="ml-md-5 mt-xs-1  form-control" id="">
-                <option value="Company A">company A</option>
-                <option value="Company B">company B</option>
+              <select v-model="search_params.company_name" name="" class="ml-md-5 mt-xs-1  form-control" id="">
+                <option  v-for="(item,i) in companies" :key="i" :value="item.CompanyName">{{item.CompanyName}}</option>
 
               </select>
              </div>
@@ -57,15 +28,15 @@
         <b-row class="mt-2">
            <b-col md="11" class="text-mdleft">
              <div class="d-block d-md-flex">
-                <b-form-checkbox class="mt-xs-1 self-center">
+                <b-form-checkbox v-model="search_params.sign_up_flag" class="mt-xs-1 self-center">
                 Sign Up From
               </b-form-checkbox>
               <div class="ml-md-3 mt-xs-1">
-              <b-form-datepicker style="border-radius:1rem;" ></b-form-datepicker>
+              <b-form-datepicker v-model="search_params.FromDate" style="border-radius:1rem;" ></b-form-datepicker>
               </div>
               <div class="ml-1 self-center mt-xs-1" >to</div>
               <div class="ml-1 mt-xs-1">
-              <b-form-datepicker style="border-radius:1rem;" ></b-form-datepicker>
+              <b-form-datepicker  v-model="search_params.ToDate" style="border-radius:1rem;" ></b-form-datepicker>
               </div>
              </div>
 
@@ -74,7 +45,7 @@
          <b-row class="mt-2">
            <b-col md="6" class="text-md-left">
              <div class="d-block d-md-flex">
-                <b-form-checkbox class="mt-xs-1" v-model="email_filter" style="align-self:center;">
+                <b-form-checkbox class="mt-xs-1" v-model="search_params.email_flag" style="align-self:center;">
                 Email Verified
               </b-form-checkbox>
               
@@ -84,26 +55,25 @@
         </b-row>
       </div>
       <div class="mt-2 text-left d-flex">
+         <b-form-checkbox class="mt-xs-1" v-model="search_params.search_flag"  style="align-self:center;">
+
+          </b-form-checkbox>
+        <!-- v-model="search_params.sign_up_flag" -->
         <div class="self-center">
         Search
         </div>
         <div class="ml-3">
-        <input v-model="search_filter" class=" border-hids form-control col-md-12">
+        <input v-model="search_params.search"  class=" border-hids form-control col-md-12">
         </div>
       </div>
        <div class="mt-2 mb-2 text-md-right">
-         <download-csv
-          class   = "btn btn-primary"
-          :data   = "allusers"
-          name    = "filename.csv">      
-          Excel
-          </download-csv>
-        <!-- <b-button pill variant="primary" class="pr-4 pl-4">Excel</b-button> -->
-      </div>
+          <b-button size="sm" variant="primary" class="mr-3" @click="searchIt()">Search</b-button>
+          <b-button size="sm" variant="primary" @click="clearfilter()" class="pr-4 mr-3 pl-4">Clear Filters</b-button>
+          <b-button size="sm" variant="primary" @click="downloadExcel()" class="pr-4 mr-3 pl-4">Excel</b-button>
+
+        </div>
       <div>
-        <b-table striped hover :responsive="true" :fields="active_user=='Employers' ? fields:individual_fields" :items="active_user=='Employers' ? employer:users"        
-          :current-page="currentPage"
-          :per-page="perPage">
+        <b-table v-if="orderLoad==false" striped hover :responsive="true" :fields="active_user=='Employers' ? fields:individual_fields" :items="view_able_users">
             <template v-slot:head(UserId)="data">
               <span class="ex-smalls">ID</span>
             </template>
@@ -172,16 +142,20 @@
             </template>
                
               
-          </b-table>
-          <div class="float-right" >
+        </b-table>
+        <div v-else class="text-center">
+              <b-spinner  variant="primary" label="Spinning"></b-spinner>
+          </div> 
+        <div class="float-right" >
           <b-pagination
               v-model="currentPage"
               :total-rows="totalRows"
+              @change="changeUserItems"
               :per-page="perPage"
               class="my-0"
               pills
           ></b-pagination>
-          </div>
+        </div>
       </div>
 
     </b-container>
@@ -203,84 +177,125 @@ export default {
         SecondaryHeader
     },
     watch:{
-      employer(){
-            this.totalRows = this.active_user=='Employers' ? this.employer.length:this.users.length
+      fileurl(){
+        clearTimeout(this.timers);
 
       },
-      users(){
-            this.totalRows = this.active_user=='Employers' ? this.employer.length:this.users.length
-
-      }
-
     },
     computed:{
-      ...mapGetters(['employer','users','allusers']),
-      filtered_users(){
-        if(this.email_filter==''){
-          console.log('yoo')
-            return this.users.filter(item=>
-            item.LastName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.FirstName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.Email.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.UserName.toLowerCase().includes(this.search_filter.toLowerCase()) )
-
-          }
-          else{
-            const result=this.users.filter(item=>item.IsVerified==1)
-            
-            return result.filter( item=>
-            item.LastName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.FirstName.toLowerCase().includes(this.search_filter.toLowerCase())  ||
-            item.Email.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.UserName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.CompanyName.toLowerCase().includes(this.search_filter.toLowerCase()))
-
-          }
-
-      },
-      filtered_employers(){
-          if(this.email_filter==''){
-            return this.employer.filter(item=>
-            item.LastName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.FirstName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.Email.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.UserName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.CompanyName.toLowerCase().includes(this.search_filter.toLowerCase()) )
-
-          }
-          else{
-            const result=this.employer.filter(item=>item.IsVerified==1)
-            
-            return result.filter( item=>
-            item.LastName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.FirstName.toLowerCase().includes(this.search_filter.toLowerCase())  ||
-            item.Email.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.UserName.toLowerCase().includes(this.search_filter.toLowerCase()) ||
-            item.CompanyName.toLowerCase().includes(this.search_filter.toLowerCase()))
-
-          }
-
-
-
-      }
+      ...mapGetters(['employer','users','allusers','userpage','companies']),
 
     },
     created(){
-      this.fetchUsers()
+      this.fetchUsers(1)
 
     },
     methods:{
-        async fetchUsers(){
-        let {data}=await UserRepository.getusers()
-        console.log(data)
-        this.$store.commit("setAllUsers",data.data.PageData)
+      clearfilter(){
+        this.fetchUsers(1);
+        this.search_params={
+          FromDate:'',
+          ToDate:'',
+          search:'',
+          company_name:'',
+          sign_up_flag:false,
+          search_flag:false,
+          email_flag:false,
+          company_flag:false
+        }
+
+      },
+     async downloadExcel(){
+        let resp=await UserRepository.userExport()
+        this.timers=setTimeout(async () => {
+        let {data}=await UserRepository.userExportFile(resp.data.data.ExportKey)
+           if(data.data.File!=null){
+             this.fileurl=data.data.File
+             window.open(this.fileurl)
+           }
+          
+        }, 1000);
+          
+      },
+      changeUserItems(){
+        if (this.search_params.search_flag==false && this.search_params.email_flag==false
+            && this.search_params.sign_up_flag==false && this.search_params.company_flag==false ){
+              this.fetchUsers(this.currentPage)
+          }
+        else{
+                this.searchIt()
+        }
+        
+      },
+      async searchIt(){
+          if (this.search_params.search_flag==false && this.search_params.email_flag==false
+          && this.search_params.sign_up_flag==false && this.search_params.company_flag==false) {
+          this.$store.commit('setNotifications',{message:'No search filter activated',type:'error'})
+
+          }
+          else{
+
+          
+          this.orderLoad=true
+
+          var link_url=''
+           
+          if(this.search_params.sign_up_flag==true){
+            link_url=link_url+`&FromDate=${this.search_params.FromDate}&ToDate=${this.search_params.ToDate}`
+          }
+          if(this.search_params.search_flag==true){
+            link_url=link_url+`&s=${this.search_params.search}`
+
+          }
+          if(this.search_params.email_flag==true){
+            link_url=link_url+`&IsVerified=1`
+
+          }
+          if(this.search_params.company_flag==true){
+            link_url=link_url+`&CompanyName=${this.search_params.company_name}`
+
+          }
+          if(this.active_user=='Employers'){
+            link_url=link_url+`&UserRoleId=3`
+
+          }
+          if(this.active_user=='Individuals'){
+            link_url=link_url+`&UserRoleId=2`
+            
+          }
+          console.log(link_url)
+          let {data}=await UserRepository.searchAll({pagenum:this.currentPage,url:link_url})
+          this.totalRows=data.data.HitsTotal
+          this.view_able_users=data.data.PageData
+         
+          }
+          this.orderLoad=false
+          
+        },
+        async fetchUsers(pagenum) {
+          this.orderLoad=true
+          if(this.active_user=='Employers'){
+          let {data}=await UserRepository.getusers({pagenum:pagenum,id:3})
+          // console.log(data)            
+          // this.$store.commit("setAllUsers",{PageData:data.data.PageData,PageTotal:data.data.PageTotal})
+            this.view_able_users=data.data.PageData
+            this.totalRows=data.data.HitsTotal
+          }
+          else if(this.active_user=='Individuals'){
+          let {data}=await UserRepository.getusers({pagenum:pagenum,id:2})
+          // console.log(data)            
+          // this.$store.commit("setAllUsers",{PageData:data.data.PageData,PageTotal:data.data.PageTotal})
+            this.view_able_users=data.data.PageData
+            this.totalRows=data.data.HitsTotal
+          }
+         this.orderLoad=false
+
         }
     },
     data() {
         return {
+          orderLoad:false,
           active_user:'Employers',
-          email_filter:0,
-          search_filter:'',
          fields: [
           // A regular column
           'UserId',
@@ -295,6 +310,17 @@ export default {
           'StatusName'
 
         ],
+        search_params:{
+          FromDate:'',
+          ToDate:'',
+          search:'',
+          company_name:'',
+          sign_up_flag:false,
+          search_flag:false,
+          email_flag:false,
+          company_flag:false
+        },
+        view_able_users:[],
         individual_fields: [
           // A regular column
           'UserId',
@@ -308,7 +334,9 @@ export default {
           'StatusName'
 
         ],
-          totalRows: 1,
+        timers:'',
+        fileurl:'',
+        totalRows: 1,
         currentPage: 1,
         perPage: 5,
             
