02c9ef41a866907e69d259909758c19a9cf76d41
Raymond Wong <raymondwong@nexsoftech.com>
2020-05-20 03:37:48 +0800

    Added config.js as config file

--- src/Repository/Repository.js
+++ src/Repository/Repository.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import config from '../../config.js'
 
-const baseDomain = 'http://128.199.82.172:3000/';
-const baseURL = `${baseDomain}api/`
+const baseURL = config.baseURL;
+// const baseDomain = 'http://128.199.82.172:3000/';
+// const baseURL = `${baseDomain}api/`
 export default axios.create({
   baseURL,
   headers: {'Content-Type' : 'application/json' , 'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImFkbWluQGZhZHYuY29tIiwiVXNlck5hbWUiOiJhZG1pbiIsIlVzZXJSb2xlSWQiOjEsImV4cCI6MTU5MTU0MjYzMywiaWF0IjoxNTg2MzU4NjMzfQ.cuwyqU1oUPvBUamXNsTjTMfvwA5blGPyV0U1rDS2Kow` }
