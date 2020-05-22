3d5b8850989df5df0b620e7634782e2ec0dc1442
VIRGO96 <dawoodahmed696@gmail.com>
2020-05-22 12:52:09 +0500

    clear extra apis

--- src/Repository/OrderRepository.js
+++ src/Repository/OrderRepository.js
@@ -5,32 +5,14 @@ const order_resource = 'order/export';
 
 export default {
   getorders(pagenum) {
-    return Repository.get(`${order_list_resource}?Page=${pagenum}&Size=5`)
+    return Repository.get(`${order_list_resource}?Page=${pagenum}&Size=10`)
   },
   edit_order(payload) {
     return Repository.post(`${order_update_resource}`,payload);
   },
-  gePaymenttorders(payload) {
-    return Repository.get(`${order_list_resource}?Page=${payload.pagenum}&Size=5&PaymentDateFrom=${payload.from}&PaymentDateTo=${payload.to}`)
-  },
-  getCompletionorders(payload) {
-    return Repository.get(`${order_list_resource}&OrderCreatedFrom=${payload.from}&OrderCreatedTo=${payload.to}`)
-  },
-  getPaymentOrders(payload) {
-    return Repository.get(`${order_list_resource}&PaymentTypeId=${payload.PaymentTypeId}`)
-  },
-  getStatusOrders(payload) {
-    return Repository.get(`${order_list_resource}&OrderStatusId=${payload.OrderStatusId}`)
-  },
-  getPackageOrders(payload) {
-    return Repository.get(`${order_list_resource}&PackageServiceId=${payload.PackageServiceId}`)
-  },
-  getSearchOrderById(payload) {
-    return Repository.get(`${order_list_resource}?OrderNumber=${payload}`)
-  },
   // 
   searchAll(payload) {
-    return Repository.get(`${order_list_resource}?Page=${payload.pagenum}&Size=5`+payload.url)
+    return Repository.get(`${order_list_resource}?Page=${payload.pagenum}&Size=10`+payload.url)
   },
   userExport() {
     return Repository.post(`${order_resource}`);
